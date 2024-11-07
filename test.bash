753b93cdd4ac551bbacdd7ce28f525119936c065
Ryuhei Yoshida <s23C1147PD@s.chibakoudai.jp>
2024-11-07 11:45:22 +0900

    test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
 	echo ${1}行目が違うよ
@@ -6,8 +6,19 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ]|| ng "$LINENO"
-[ "$a" = 山田 ]|| ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || "$LINENO"
+[ "${out}" = "" ] || "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
+
